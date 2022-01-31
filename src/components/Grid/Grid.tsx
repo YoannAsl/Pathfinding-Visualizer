c9aaabdf2b6d437900b797e6558b280048105202
Yoann Agésilas <yoann.agesilas@gmail.com>
2022-01-31 12:09:00 +0100

    refactor: replace default export by named export

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -2,6 +2,4 @@ import React from 'react';
 import './Grid.css';
 import { GridProps } from '../../types';
 
-export function Grid({ children }: GridProps) {
-    return <main>{children}</main>;
-}
+export const Grid = ({ children }: GridProps) => <main>{children}</main>;
